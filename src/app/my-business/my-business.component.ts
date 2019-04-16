ac1954d0a97f73d1999a2594387dd2202fe2e04e
preciousky <1873685019@qq.com>
2019-04-16 20:17:52 +0800

    20190416

--- src/app/my-business/my-business.component.ts
+++ src/app/my-business/my-business.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-my-business',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyBusinessComponent implements OnInit {
 
-  constructor() { }
+  selectedNum: any;
+  locationUrl: any;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+
+    var businessPart= this.route.snapshot.paramMap.get('part');
+    if ( businessPart == 'papers'){
+      this.selectedNum = 0;
+    } else
+    if ( businessPart == 'drawing'){
+      this.selectedNum = 1;
+    } else
+    if ( businessPart == 'paying'){
+      this.selectedNum = 2;
+    }
+    
+    console.log(businessPart);
   }
 
 }
--- src/app/new-business/new-business.component.ts
+++ src/app/new-business/new-business.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-new-business',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewBusinessComponent implements OnInit {
 
-  constructor() { }
+  selectedNum :any;
+
+  constructor( private route: ActivatedRoute) { }
 
   ngOnInit() {
+    var businessPart= this.route.snapshot.paramMap.get('part');
+    if ( businessPart == 'discounting'){
+      this.selectedNum = 0;
+    } else
+    if ( businessPart == 'endorsing'){
+      this.selectedNum = 1;
+    } else
+    if ( businessPart == 'accepting'){
+      this.selectedNum = 2;
+    } else
+    if ( businessPart == 'acceptingForHonour'){
+      this.selectedNum = 3;
+    }
   }
-
 }
